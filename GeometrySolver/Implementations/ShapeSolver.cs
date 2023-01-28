03e60a052b65ab1db6e4a86ff91fef6fc9d9e09a
skalerr <rob06121@gmail.com>
2023-01-28 17:31:28 +0500

    Добавил запрос SQl в комментарии

--- GeometrySolver/Implementations/ShapeSolver.cs
+++ GeometrySolver/Implementations/ShapeSolver.cs
@@ -16,3 +16,10 @@ namespace ClassLibrary1.Implementations
     }
 }
 
+
+// SELECT Product.Name, Category.Name
+//     FROM Product
+//     LEFT JOIN ProductCategory ON Product.ID = ProductCategory.ProductID
+// LEFT JOIN Category ON ProductCategory.CategoryID = Category.ID
+// Этот запрос использует LEFT JOIN, чтобы включить в результат все продукты, даже если у них нет соответствующей категории.
+// Он соединяет таблицы Product, ProductCategory и Category, чтобы получить имена продуктов и категорий.
